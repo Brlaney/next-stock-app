dabd0768a0596de10f1c5ef46ec1ff6c000f624d
brlaney42 <brlaney@outlook.com>
2021-10-29 21:35:08 -0500

    pushing changes

--- src/pages/cryptocurrency/index.tsx
+++ src/pages/cryptocurrency/index.tsx
@@ -1,95 +1,60 @@
 import * as React from 'react';
+import Image from 'next/image';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { cryptoEndpoint } from '@/lib/utils/crypto';
+import { cryptoEndpoint, append } from '@/lib/utils/crypto';
 import { ICrypto } from '@/lib/types';
 import GoBack from '@/components/global/GoBack';
-import styles from '@/styles/pages/Tests.module.scss';
+import styles from '@/styles/pages/Crypto.module.scss';
 
 const Cryptocurrency = ({ cryptos }:
   InferGetStaticPropsType<typeof getStaticProps>) => {
-  // const [cryptoList] = React.useState(cryptos);
-  // const [dataList] = React.useState(data);
   const endpoint = '/';
 
   return (
     <div className={styles.container}>
 
       <GoBack link={endpoint} />
+
+      {/* Header */}
       <h2 className={styles.title}>
         Cryptocurrency api
       </h2>
+      <h3 className={styles.subheader}>
+        Test response
+      </h3>
 
-      <div className={styles.row}>
-        <div className={styles.col1}>
-          <ul className='uk-list uk-list-square'>
-            <li>id: </li>
-            <li>Symbol: </li>
-            <li>Name: </li>
-            <li>Current price: </li>
-            <li>market cap: </li>
-            <li>market cap rank: </li>
-            <li>fully diluted valuation: </li>
-            <li>total volume: </li>
-            <li>high 24h: </li>
-            <li>low 24h: </li>
-            <li>price change 24h: </li>
-            <li>price change percentage 24h: </li>
-            <li>market cap change 24h: </li>
-            <li>market cap change percentage 24h: </li>
-            <li>circulating supply: </li>
-            <li>total supply: </li>
-            <li>max supply: </li>
-            <li>Ath: </li>
-            <li>Ath change percentage: </li>
-            <li>Ath date: </li>
-            <li>Atl: </li>
-            <li>Atl hange percentage: </li>
-            <li>Atl date: </li>
-            <li>Roi </li>
-            <li>Last updated: </li>
-          </ul>
-        </div>
-        <div className={styles.col2}>
-          <ul className='uk-list uk-list-square'>
-            {cryptos.map((crypto: ICrypto) => (
-              <>
-                <li>{crypto.id}</li>
-                <li>{crypto.symbol}</li>
-                <li>{crypto.name}</li>
-                <li>{crypto.current_price}</li>
-                <li>{crypto.market_cap}</li>
-                <li>{crypto.market_cap_rank}</li>
-                <li>{crypto.fully_diluted_valuation}</li>
-                <li>{crypto.total_volume}</li>
-                <li>{crypto.high_24h}</li>
-                <li>{crypto.low_24h}</li>
-                <li>{crypto.price_change_24h}</li>
-                <li>{crypto.price_change_percentage_24h}</li>
-                <li>{crypto.market_cap_change_24h}</li>
-                <li>{crypto.market_cap_change_percentage_24h}</li>
-                <li>{crypto.circulating_supply}</li>
-                <li>{crypto.total_supply}</li>
-                <li>{crypto.max_supply}</li>
-                <li>{crypto.ath}</li>
-                <li>{crypto.ath_change_percentage}</li>
-                <li>{crypto.ath_date}</li>
-                <li>{crypto.atl}</li>
-                <li>{crypto.atl_change_percentage}</li>
-                <li>{crypto.atl_date}</li>
-                <li>{crypto.roi}</li>
-                <li>{crypto.last_updated}</li>
-              </>
-            ))}
-          </ul>
-        </div>
+      <div className={styles.box}>
+        {cryptos.map((crypto: ICrypto) => (
+          <div key={crypto.id} className={styles.card}>
+            <div className={styles.imgBox}>
+              <Image
+                className={styles.image}
+                src='/${crypto.image}'
+                layout='fixed'
+                alt='crypto-icon'
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className={styles.row1}>
+              {crypto.name}
+            </div>
+            <div className={styles.row1}>
+              {crypto.symbol}
+            </div>
+            <div className={styles.row2}>
+              Current value: <span className={styles.span}>
+                {crypto.current_price}
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async _context => {
-  const append = 'markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
-
   const res = await fetch(`${cryptoEndpoint}/${append}`);
   const cryptos: ICrypto[] = await res.json();
 
